15c4016f83084018294b08051b996d511702ca4b
Han <hyutong0825@gmail.com>
2025-06-20 06:11:15 +0000

    [automatic commit] 2025-06-20 06:11:15 UTC 203.220.22.88

--- part2/routes/loginRoutes.js
+++ part2/routes/loginRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../models/db'); // 使用你已有的 db.js
+const pool = require('../models/db'); 
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
