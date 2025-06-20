fe392590ab6fc9eb9235b3d10e165e8e709beaed
Han <hyutong0825@gmail.com>
2025-06-20 06:09:13 +0000

    [automatic commit] 2025-06-20 06:09:13 UTC 203.220.22.88

---
+++ part2/routes/loginRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../models/db'); 
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const [rows] = await pool.query(
+      'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
+      [username, password]
+    );
+
+    if (rows.length === 1) {
+      const user = rows[0];
+
+      req.session.user = {
+        id: user.user_id,
+        username: user.username,
+        role: user.role
+      };
+
+      if (user.role === 'owner') {
+        res.redirect('/owner-dashboard.html');
+      } else {
+        res.redirect('/walker-dashboard.html');
+      }
+    } else {
+      res.send('❌ Invalid credentials');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
