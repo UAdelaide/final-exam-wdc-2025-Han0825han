d6fda4f37e0fd43d0aab2c799e9cd4cfa74aec1d
Han <hyutong0825@gmail.com>
2025-06-20 01:55:46 +0000

    [automatic commit] 2025-06-20 01:55:46 UTC 203.220.22.88

--- part1/app.js
+++ part1/app.js
@@ -74,12 +74,6 @@ app.get('/api/walkers/summary', async (req, res) => {
 });
 
 
-
-
-app.get('/', (req, res) => {
-  res.send('✅ Express is running!');
-});
-
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
