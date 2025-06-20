91c54c04f1defc0f81596d04727c6bff916f64b2
Han <hyutong0825@gmail.com>
2025-06-20 11:30:49 +0000

    [automatic commit] 2025-06-20 11:30:49 UTC 203.220.22.88

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -29,6 +29,7 @@ router.post('/register', async (req, res) => {
 });
 
 router.get('/me', (req, res) => {
+  console.log('Current session:', req.session);
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
