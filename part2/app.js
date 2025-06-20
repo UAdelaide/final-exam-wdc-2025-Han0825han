afba27b50edf4bf29444c4f94f2d1d391406d792
Han <hyutong0825@gmail.com>
2025-06-20 11:35:55 +0000

    [automatic commit] 2025-06-20 11:35:55 UTC 203.220.22.88

--- part2/app.js
+++ part2/app.js
@@ -25,15 +25,6 @@ app.use('/api', userRoutes);
 app.use('/', loginRoutes);
 
 
-window.addEventListener('DOMContentLoaded', async () => {
-  try {
-    const meRes = await fetch('/api/users/me');
-    const me = await meRes.json();
-    console.log('当前 session 用户:', me);
-  } catch (e) {
-    console.error('获取 session 用户失败:', e);
-  }
-});
 // Export the app
 module.exports = app;
 
